6d18c5e6edd8458398e0f94f1560517141411a61
JohnSalt <johnsalt028@gmail.com>
2020-10-21 10:42:56 -0400

    Added Enemy Health System

--- src/StormTheHouse.java
+++ src/StormTheHouse.java
@@ -8,7 +8,6 @@ public class StormTheHouse extends PApplet {
     Random r = new Random();
     int day = 0, textSizeModifier = 100, power = 1;
     Window window = new Window(this);
-    Enemy e = new Enemy(8,50);
     ArrayList<Enemy> enemies = new ArrayList<>();
     private ArrayList<Integer> speeds;
     private boolean isMouseEnabled = true;
@@ -34,6 +33,7 @@ public class StormTheHouse extends PApplet {
         base.drawBase(this);
         textSize(30);
         text("Day: " + day, window.getWindowWidth()-textSizeModifier, 35);
+
         if (enemies.size() == 0) {
             System.out.println("Enemies list empty");
             day++;
@@ -48,23 +48,33 @@ public class StormTheHouse extends PApplet {
         }
         moveAll();
         checkWallCollision();
+        damageWall();
+        text("Wall: " + base.getHealth() + "/100",window.getWindowWidth()-230,75);
+        if (base.getHealth() < 0) {
+            text("Wall: " + 0 + "/100",window.getWindowWidth()-230,75);
+        }
+        updateHealthBar();
         checkForLoss();
     }
     public void mousePressed() {
         if (isMouseEnabled) {
             for (int i = 0; i < enemies.size(); i++) {
                 if (mouseX >= enemies.get(i).getCurrentX() - 15 && mouseX <= enemies.get(i).getCurrentX() + 25 && mouseY >= enemies.get(i).getStartingY() && mouseY <= enemies.get(i).getStartingY() + 50) {
-                    enemies.remove(i);
+                    enemies.get(i).setHealth(1);
+                    if (enemies.get(i).getHealth() == 0) {
+                        enemies.remove(i);
+                    }
                 }
             }
         }
     }
     public void checkForLoss() {
         for (int i = 0; i < enemies.size(); i++) {
-            if (enemies.get(i).getCurrentX() >= window.getWindowWidth()) {
+            if (base.getHealth() <= 0) {
                 noLoop();
                 isMouseEnabled = false;
-                text("GAME OVER", window.getWindowWidth() / 2 - 40, window.getWindowHeight() / 2 + 5);
+                textSize(30);
+                text("GAME OVER", window.getWindowWidth() / 2 - 105, window.getWindowHeight() / 2 + 5);
             }
         }
     }
@@ -80,6 +90,11 @@ public class StormTheHouse extends PApplet {
         if (key == ESC) {
             exit();
         }
+        if (key == 'k') {
+            for (int i = 0; i < enemies.size(); i++) {
+                enemies.remove(i); //removes enemies DEBUG
+            }
+        }
     }
 
     public void createEnemies() {
@@ -93,7 +108,7 @@ public class StormTheHouse extends PApplet {
             enemies.get(i).move();
         }
     }
-    public void checkWallCollision() {
+    public void checkWallCollision() {  //makes the wall a barrier, changes speed to 0
         int wallX;
         for (int i = 0; i < enemies.size(); i++) {
             wallX = (enemies.get(i).getStartingY()+1200)/base.getSlope();
@@ -101,6 +116,23 @@ public class StormTheHouse extends PApplet {
             if (enemies.get(i).getCurrentX() >= wallX) {
                 enemies.get(i).setSpeed(0);
             }
+        }
+    }
+    public void damageWall() {
+        for (int i = 0; i < enemies.size(); i++) {
+            if (enemies.get(i).getSpeed() == 0) {
+                if (frameCount%10 == 0) {
+                    base.setHealth(1);
+                }
+            }
+        }
+    }
+    public void updateHealthBar() {
+        float healthBarLength = (float)base.getHealth()/100*140;
+        stroke(0);
+        fill(207, 209, 88);
+        if (healthBarLength >= 0) {
+            rect(window.getWindowWidth() / 2 - 70, 10, healthBarLength, 20);
         }
     }
 }
