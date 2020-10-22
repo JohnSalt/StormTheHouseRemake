3727a2289e946f001b7349c87b1d63fc5706ea04
JohnSalt <johnsalt028@gmail.com>
2020-10-22 10:24:51 -0400

    Created

---
+++ src/GameTimer.java
@@ -0,0 +1,32 @@
+import processing.core.PApplet;
+
+public class GameTimer {
+    private long startTime;
+    boolean isRunning;
+
+    public void GameTimer() {
+        startTime = 0;
+        isRunning = false;
+    }
+
+    public void startTimer(PApplet parent) {
+        isRunning = true;
+        startTime = parent.millis();
+    }
+
+    public void stopTimer() {
+        isRunning = false;
+    }
+
+    public void setIsRunning(boolean isRunning) {
+        this.isRunning = isRunning;
+    }
+
+    public boolean getIsRunning() {
+        return isRunning;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+}
